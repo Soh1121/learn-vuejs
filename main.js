d762a8918f2a645024faf9b5b208dc002d54222b
Soh1121 <satou.shg@gmail.com>
2019-09-03 09:32:41 +0900

    [update]オブジェクトの定義
    オブジェクトを繰り返し処理で表示するため

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var app = new Vue({
     // options
     el: '#app',
-
+    data: {
+        user: {
+            firstName: 'Taro',
+            lastName: 'Yamada',
+            age: 28
+        }
+    }
 })
