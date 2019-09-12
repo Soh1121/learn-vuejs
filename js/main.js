188ceba827c73fe990d4e00b4f4a4ffaf7572f68
Soh1121 <satou.shg@gmail.com>
2019-09-12 21:04:08 +0900

    [update]dataオブジェクトに入れるところまで

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ const app = new Vue({
     },
     mounted: function() {
         axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function(response){
-            console.log(response.data.bpi)
-            console.log(response.data.bpi.USD.rate_float)
-        })
+            // console.log(response.data.bpi)
+            // console.log(response.data.bpi.USD.rate_float)
+            this.bpi = response.data.bpi
+        }.bind(this))
     }
 })
