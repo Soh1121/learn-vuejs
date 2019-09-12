381722b839ca0fcf8a61b2da52ec1d5585a09a00
Soh1121 <satou.shg@gmail.com>
2019-09-12 21:42:59 +0900

    [update]ロード状態を実装
    ローディング画面を表示するため

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ const app = new Vue({
     el: '#app',
     data: {
         bpi: null,
-        hasError: false
+        hasError: false,
+        loading: true
     },
     mounted: function() {
         axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function(response){
@@ -12,6 +13,8 @@ const app = new Vue({
         }.bind(this)).catch(function(error) {
             console.log(error)
             this.hasError = true
+        }.bind(this)).finally(function() {
+            this.loading = false
         }.bind(this))
     },
     filters: {
