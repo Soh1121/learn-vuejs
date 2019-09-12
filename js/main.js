0cd0b1a0a4d8d10346c0b85ad8f0d6af92ddd52d
Soh1121 <satou.shg@gmail.com>
2019-09-12 21:02:06 +0900

    [update]ほしい部分の値を取得

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const app = new Vue({
     },
     mounted: function() {
         axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function(response){
-            console.log(response)
+            console.log(response.data.bpi)
         })
     }
 })
