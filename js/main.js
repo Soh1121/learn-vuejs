afb4335e295b95ea1b3d2a5c5298e6eadc92f3bb
Soh1121 <satou.shg@gmail.com>
2019-09-17 20:03:45 +0900

    [clean]各ファイルをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 const app = new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Vue.js!'
-    },
-    methods: {
-        clickHandler: function(event) {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
 })
