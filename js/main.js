d90108578e714c3bf340e76a53c52816884175b0
Soh1121 <satou.shg@gmail.com>
2019-09-18 23:30:51 +0900

    [update]ランダムな数値を返す算出プロパティとメソッドを作成
    算出プロパティとメソッドの比較のため

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 const app = new Vue({
     el: '#app',
+    computed: {
+        computedNumber: function () {
+            return Math.random()
+        }
+    },
+    methods: {
+        methodsNumber: function () {
+            return Math.random()
+        }
+    }
 })
