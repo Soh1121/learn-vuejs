afd9dd85517dfafe05a9a90492059c0ca7929da6
Soh1121 <satou.shg@gmail.com>
2019-09-18 23:01:19 +0900

    [update]算出プロパティ内で処理を定義
    可読性・再利用性を高めるため

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ const app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!'
+    },
+    computed: {
+        reversedMessage: function() {
+            return message.split('').reverse().join('')
+        }
     }
 })
