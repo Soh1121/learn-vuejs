c125c2472dc323b0143c04a4c75cbe10d44feb25
Soh1121 <satou.shg@gmail.com>
2019-09-18 23:22:46 +0900

    [update]算出プロパティを定義
    getter・setterを理解するため

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 const app = new Vue({
     el: '#app',
+    data: {
+        basePrice: 100
+    },
+    computed: {
+        taxIncludedPrice: {
+            get: function () {
+                return this.basePrice * 1.08
+            },
+            set: function(taxIncludedPrice) {
+                this.basePrice = taxIncludedPrice / 1.08
+            }
+        }
+    }
 })
