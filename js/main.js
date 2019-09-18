93009e92d6aff868a16049a63fec2466c8226715
Soh1121 <satou.shg@gmail.com>
2019-09-18 22:27:16 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-Vue.filter('numberFormat', function(value) {
-    return value.toLocaleString()
-})
-
 const app = new Vue({
     el: '#app',
-    data: {
-        price: 29800
-    },
-/*     filters: {
-        numberFormat: function (value) {
-            return value.toLocaleString()
-        }
-    } */
 })
