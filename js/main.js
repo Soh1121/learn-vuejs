eb8fb98a3ad9513769eb4e79b4032efbfa33f3a4
Soh1121 <satou.shg@gmail.com>
2019-09-18 22:37:57 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-Vue.filter('toUSD', function (jpy) {
-    return jpy / 100
-})
-
-Vue.filter('numberFormat', function (value) {
-    return value.toLocaleString()
-})
-
 const app = new Vue({
     el: '#app',
-    data: {
-        jpyPrice: 298000
-    }
 })
