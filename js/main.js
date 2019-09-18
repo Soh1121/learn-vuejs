c0bd3f680dacbc341eee8242fd0a9326e3a89512
Soh1121 <satou.shg@gmail.com>
2019-09-18 23:28:33 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
 const app = new Vue({
     el: '#app',
-    data: {
-        basePrice: 100
-    },
-    computed: {
-        taxIncludedPrice: {
-            get: function () {
-                return parseInt(this.basePrice * 1.08)
-            },
-            set: function(taxIncludedPrice) {
-                this.basePrice = Math.ceil(taxIncludedPrice / 1.08)
-            }
-        }
-    }
 })
