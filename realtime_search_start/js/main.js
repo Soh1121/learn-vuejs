75760ea69ebf0bd15bc4834242272d182b631d89
Soh1121 <satou.shg@gmail.com>
2019-09-19 23:13:43 +0900

    [update]APIを叩くmethodを用意
    APIからデータを取得し、ユーザーに情報を表示するため

--- realtime_search_start/js/main.js
+++ realtime_search_start/js/main.js
@@ -12,6 +12,29 @@ var app = new Vue({
 
     },
     methods: {
+        getAnswer: function () {
+            if(this.keyword === '') {
+                this.items = null
+                return
+            }
 
+            this.message = 'Loading...'
+            const vm = this
+            const params = {
+                page: 1,
+                per_page: 20,
+                query: this.keyword
+            }
+            axios.get('https://qiita.com/api/v2/items', { params })
+                .then(function (response) {
+                    console.log(response)
+                })
+                .catch(function (error) {
+                    vm.message = `Error!${error}`
+                })
+                .finally(function () {
+                    vm.message = ''
+                })
+        }
     }
 })
