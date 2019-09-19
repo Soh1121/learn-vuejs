340517bce262d58fdaa13dd0db24fa16d13ec945
Soh1121 <satou.shg@gmail.com>
2019-09-19 22:06:33 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-/* const app = new Vue({
-    el: '#app',
-    data: {
-        firstName: '',
-        lastName: '',
-        fullName: ''
-    },
-    watch: {
-        firstName: function (value) {
-            this.fullName = `${value} ${this.lastName}`
-        },
-        lastName: function (value) {
-            this.fullName = `${this.firstName} ${value}`
-        }
-    }
-})
- */
-
 const app = new Vue({
     el: '#app',
-    data: {
-        firstName: '',
-        lastName: '',
-    },
-    computed: {
-        fullName: function () {
-            return `${this.firstName} ${this.lastName}`
-        }
-    }
 })
