f851f3d20c3c4c31b892f38b7d0c185a9c74ca0e
Soh1121 <satou.shg@gmail.com>
2019-09-19 21:30:28 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
 const app = new Vue({
     el: '#app',
-    computed: {
-        computedNumber: function () {
-            console.log('computed!')
-            return Math.random()
-        }
-    },
-    methods: {
-        methodsNumber: function () {
-            console.log('methods!')
-            return Math.random()
-        }
-    }
 })
