176ee58dad0bad7477cf3b761eed75200e79e341
Soh1121 <satou.shg@gmail.com>
2019-09-19 21:36:24 +0900

    [update]監視プロパティを設置
    テキストボックスに変化があれば前後をコンソールに表示するため

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ const app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue.js!'
+    },
+    watch: {
+        message: (newValue, oldValue) => {
+            console.log(`new: ${newValue}, old: ${oldValue}`)
+        }
     }
 })
