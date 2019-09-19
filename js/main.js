9c499073725b51a71dc1ae380a3b1ff2381406aa
Soh1121 <satou.shg@gmail.com>
2019-09-19 21:40:03 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 const app = new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Vue.js!'
-    },
-    watch: {
-        message: (newValue, oldValue) => {
-            console.log(`new: ${newValue}, old: ${oldValue}`)
-        }
-    }
 })
