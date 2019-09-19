cb39fa8b7c9c4350f54f297ac38689270ed7b09b
Soh1121 <satou.shg@gmail.com>
2019-09-19 21:47:23 +0900

    [update]kmに変化があった場合m、mmを変換
    kmから単位変換するため

--- js/main.js
+++ js/main.js
@@ -4,5 +4,12 @@ const app = new Vue({
         km: 0,
         m: 0,
         mm: 0
+    },
+    watch: {
+        km: function(value) {
+            this.km = value
+            this.m = value * 1000
+            this.mm = value * 1000000
+        }
     }
 })
