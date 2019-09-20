eacc2fbb5be171396be77ffd6cd05f3a1f3d9a9a
Soh1121 <satou.shg@gmail.com>
2019-09-20 22:14:21 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
 const app = new Vue({
     el: '#app',
-    data: {
-        largeClass: {
-            large: true,
-            'bg-gray': true
-        },
-        dangerClass: {
-            'text-danger': true
-        },
-        isLarge: true
-    }
 })
