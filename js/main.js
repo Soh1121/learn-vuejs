dcec688ba8e27b64c5d4a06a64d64c041a6a9277
Soh1121 <satou.shg@gmail.com>
2019-09-20 21:55:57 +0900

    [update]dataオプションを用意
    三項演算子を使うため

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 const app = new Vue({
     el: '#app',
+    data: {
+        largeClass: {
+            large: true,
+            'bg-gray': true
+        },
+        dangerClass: {
+            'text-denger': true
+        },
+        isLarge: true
+    }
 })
