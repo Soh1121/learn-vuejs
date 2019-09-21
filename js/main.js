d2f88d9746fe9096daa20126703ac0a8878a9718
Soh1121 <satou.shg@gmail.com>
2019-09-21 12:52:48 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
 const app = new Vue({
     el: '#app',
-    data: {
-        message: ''
-    },
-    methods: {
-        clickHandler: function ($event, message) {
-            this.message = message
-            console.log($event)
-        }
-    }
 })
