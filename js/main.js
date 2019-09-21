2c154c70eb55de85879991c71799eaa3e1a4f514
Soh1121 <satou.shg@gmail.com>
2019-09-21 13:00:49 +0900

    [clean]コードをクリーンアップ
    学習の準備のため

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 const app = new Vue({
     el: '#app',
-    data: {
-        message: ''
-    },
-    methods: {
-        clickHandler: function () {
-            this.message = new Date().toLocaleTimeString()
-        }
-    }
 })
