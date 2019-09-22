43069f629bf494f80154934f522697dab9c05056
Soh1121 <satou.shg@gmail.com>
2019-09-22 17:07:40 +0900

    [update]コンポーネントを定義
    コンポーネントのオプションを知るため

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+Vue.component('button-counter', {
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">{{ count }}</button>'
+})
 const app = new Vue({
     el: '#app',
 })
