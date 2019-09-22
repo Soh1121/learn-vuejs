d470aca486ffa16154abd2beb0d3f2421ff87e6f
Soh1121 <satou.shg@gmail.com>
2019-09-22 17:12:24 +0900

    [update]テンプレートを複数要素から成り立たせる場合は、divなどでひとつの要素とする
    テンプレート内には要素を一つしか置けないため

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ Vue.component('button-counter', {
             count: 0
         }
     },
-    template: '<button v-on:click="count++">{{ count }}</button>'
+    template: '<div><span>count:</span><button v-on:click="count++">{{ count }}</button><div>'
 })
 const app = new Vue({
     el: '#app'
